5b2b44a5a630a147e1893f02d6e1cab18b4cef70
mallone.nogueira <mallone.nogueira@SHX0532-LINUX.shxinfor.local>
2020-02-11 17:08:41 -0300

    Updates

---
+++ precache-manifest.428889302d2fd126df8dd670bd97bbcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4b2c303d281e5658f66473f00fea7f",
+    "url": "/geforce-now-games/index.html"
+  },
+  {
+    "revision": "7c7a10923235667422b0",
+    "url": "/geforce-now-games/static/css/main.1b437edd.chunk.css"
+  },
+  {
+    "revision": "de80be4d3ae7549c41cc",
+    "url": "/geforce-now-games/static/js/2.cb0c6d3e.chunk.js"
+  },
+  {
+    "revision": "7c7a10923235667422b0",
+    "url": "/geforce-now-games/static/js/main.a5d2463d.chunk.js"
+  },
+  {
+    "revision": "c7eade12bbe056b56b0d",
+    "url": "/geforce-now-games/static/js/runtime~main.b0c3112b.js"
+  }
+]);
