dac48d2cee94de2bbfb5070c565426a86da691ff
Mallone <mallone.nogueira@exactaworks.com.br>
2020-06-30 14:30:12 -0300

    Updates

---
+++ precache-manifest.f7c3b30de7c0ff7e477a033022fbb260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d80e704bdc7f62d20e10f4caaff5b2",
+    "url": "/geforce-now-games/index.html"
+  },
+  {
+    "revision": "c300daf611cd7cc9d546",
+    "url": "/geforce-now-games/static/css/main.d71d37ce.chunk.css"
+  },
+  {
+    "revision": "0cf2bb196d9b55ef0954",
+    "url": "/geforce-now-games/static/js/2.287d3a16.chunk.js"
+  },
+  {
+    "revision": "c300daf611cd7cc9d546",
+    "url": "/geforce-now-games/static/js/main.50834b6e.chunk.js"
+  },
+  {
+    "revision": "c7eade12bbe056b56b0d",
+    "url": "/geforce-now-games/static/js/runtime~main.b0c3112b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/geforce-now-games/precache-manifest.b55c3021935d7e5de5aae1d975444269.js"
+  "/geforce-now-games/precache-manifest.f7c3b30de7c0ff7e477a033022fbb260.js"
 );
 
 self.addEventListener('message', (event) => {
