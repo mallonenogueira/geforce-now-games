259226ece71ac10dbfaafe8d543741326b49f564
Mallone <mallone.nogueira@exactaworks.com.br>
2020-06-30 18:01:31 -0300

    feat: add empty message

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,12 +7,18 @@ export default function Application({ loading, games }) {
   return (
     <main className="application row">
       <ApplicationLoading loading={loading}>
-        {games.map((game) => (
-          <div className="col-4" key={game.id}>
-            <CardGame game={game} />
-          </div>
-        ))}
+        {!!games.length &&
+          games.map((game) => (
+            <div className="col-4" key={game.id}>
+              <CardGame game={game} />
+            </div>
+          ))}
+        {!games.length && <ApplicationEmpty />}
       </ApplicationLoading>
     </main>
   );
+}
+
+function ApplicationEmpty() {
+  return <p className="application-empty">Nenhum jogo foi encontrado.</p>;
 }
